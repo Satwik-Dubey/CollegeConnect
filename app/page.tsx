a82f5f981f438042e89363e31a772a354123c370
SatwikDubey <dubeysatwik0@gmail.com>
2025-02-15 00:02:31 +0530

    updated some features

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,30 @@
+import CategoriesList from "@/components/CategoriesList";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <section className="bg-black w-full bg-green-400 min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
-        <h1 className="uppercase text-white px-6 py-3 font-work-sans font-extrabold sm:text-[54px] sm:leading-[64px] text-[36px]  max-w text-center my-5">Home</h1>
-
+      <section className="pink_container">
+        <h1 className="heading">
+          Create Events, Join Events, <br />
+          Post Freely, Grab Notes !
+        </h1>
+        <p className="sub-heading !max-w-3xl font-work-sans font-bold">
+          Create Events, Join the Fun, Post Your Thoughts, and Collect Notes Effortlessly.
+        </p>
       </section>
+      <section className="section_container" > 
+        <p className="text-30-bold text-center font-work-sans font-extrabold">All Post </p>
+      </section>
+
+      <section className=" my-10 ml-10">
+        <CategoriesList/>
+      </section>
+
       
 
     </>
   );
 }
+
+{/* <h1 className="uppercase text-white px-6 py-3 font-work-sans font-extrabold sm:text-[54px] sm:leading-[64px] text-[36px]  max-w text-center my-5">Home</h1> */}
