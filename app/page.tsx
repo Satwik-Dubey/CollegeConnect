5fa0553966dba7a6fe0f59ece5ea71f0267d4eb9
SatwikDubey <dubeysatwik0@gmail.com>
2025-02-14 21:27:54 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <section className="bg-black w-full bg-green-400 min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6">
+        <h1 className="uppercase text-white px-6 py-3 font-work-sans font-extrabold sm:text-[54px] sm:leading-[64px] text-[36px]  max-w text-center my-5">Home</h1>
+
+      </section>
+      
+
+    </>
+  );
+}
