c37715e07201a12424346f5648b24d8f31f7a296
SatwikDubey <dubeysatwik0@gmail.com>
2025-02-18 04:21:12 +0530

    rendered posts

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,7 +1,54 @@
-export default function Post(){
+import Image from "next/image"
+import Link from "next/link"
+interface PostProps {
+    id:string,
+    author:string,
+    date:string,
+    thumbnail?:string,
+    authorEmail?:string,
+    image?:string,
+    title:string,
+    content:string,
+    category?:string,
+    description:string,
+    links?:string[],
+
+}
+
+
+
+
+export default function Post({
+    id,
+    author, 
+    description, 
+    links, 
+    date: datePublished, 
+    title, 
+    content, 
+    category, 
+    thumbnails, 
+    authorEmail, 
+    image,
+}: PostProps){
     return(
         <>
-        post
+        
+        <li  className="startup_card group">
+            <div className="flex-between">
+                <p className="startup_card_date">
+                    {title}
+                </p>
+            </div>
+            <div className="flex-between mt-5 gap-5">
+                <div className="flex-1">
+                    <Link href={`/user/${id}`}></Link>
+                    <p className="text-16-medium line-clamp-1">{author}</p>
+                </div>
+            </div>
+        </li>
+
+        
         </>
     )
 }
