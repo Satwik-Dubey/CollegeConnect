1530bca756a452840d2f4d16fb8bf03ca6e0c5f4
SatwikDubey <dubeysatwik0@gmail.com>
2025-02-18 04:21:44 +0530

    updated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['img.daisyui.com', 'img.icons8.com', 'images.pexels.com', 'images.unsplash.com']
+  }
 };
 
 export default nextConfig;
