bb19440f8089fcc4dd2f4e446a618d293c928046
SatwikDubey <dubeysatwik0@gmail.com>
2025-02-18 04:20:45 +0530

    blog posts

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import CategoriesList from "@/components/CategoriesList";
 import Image from "next/image";
 import { postData } from "@/data";
 import Startupcard from "@/components/Startupcard";
+import Post from "@/components/Post";
 
 export default function Home() {
   return (
@@ -15,28 +16,53 @@ export default function Home() {
           Create Events, Join the Fun, Post Your Thoughts, and Collect Notes Effortlessly.
         </p>
       </section>
+      
       <section className="section_container" > 
         <p className="text-30-bold text-center font-work-sans font-extrabold">All Post </p>
         <section className=" ml-96 mt-20">
         <CategoriesList/>
         </section>
-        {/* <ul className="mt-7 card_grid">
+          <section>
           {postData && postData.length > 0 ? (
-            postData.map((post: StartupCardType, index: number) => (
+              postData.map((post) => (
+                <Post
+                  key={post.id}
+                  id={post.id}
+                  author={post.author}
+                  description={post.description}
+                  links={post.links || []}
+                  datePublished={post.datePublished}
+                  title={post.title}
+                  content={post.content}
+                  category={post.category}
+                  thumbnail={post.thumbnails || []}
+                  authorEmail={'test@gmail.com'}
+                  image={post.image}
+                />
+              ))
+            ) : (
+              <div>No Post to Display</div>
+            )}
+        </section>
+
+      </section>
+
+    </>
+  );
+}
+
+
+
+
+
+
+
+                {/* <ul className="mt-7 card_grid">
+          {postData && postData.length > 0 ? (
+            postData.map((post: StartupCardType) => (
               <Startupcard key={post.id} post={post} />
             ))
           ) : (
             <p className="no_results">No Posts Found</p>
           )}
         </ul> */}
-      </section>
-
-
-
-      
-
-    </>
-  );
-}
-
-{/* <h1 className="uppercase text-white px-6 py-3 font-work-sans font-extrabold sm:text-[54px] sm:leading-[64px] text-[36px]  max-w text-center my-5">Home</h1> */}
